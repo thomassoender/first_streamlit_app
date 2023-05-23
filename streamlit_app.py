5eb4d643f8fbed437c8e7692a0503d8f6a041273
thomassoender <thomassoender@gmail.com>
2023-05-23 13:09:46 +0200

    Update streamlit_app.py
     Choose a Few Fruits to Set a Good Example

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# 🥋 Choose a Few Fruits to Set a Good Example
+# We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocdao','Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
